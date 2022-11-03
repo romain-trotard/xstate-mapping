915c6e4c61688c68ce083a2ab727f200583d6335
Romain Trotard <romain.trotard@gmail.com>
2022-11-03 08:51:36 +0100

    Add load more feature

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { useState } from 'react';
 
 const VALUES = ['one', 'two', 'three'];
 
-function fakeAPI(search: string = '') {
+function fakeAPI({ search = '', page = 0 }: { search?: string, page?: number } = {}) {
     return new Promise<string[]>(resolve => {
         setTimeout(() => {
-            resolve(VALUES.map(value => `${search}${value}`));
+            resolve(VALUES.map(value => `${search}${value}${page}`));
         }, 2000);
     });
 }
@@ -17,25 +17,47 @@ const toggleMachine = createMachine({
     predictableActionArguments: true,
     id: 'toggle',
     context: {
+        currentPageNumber: 0,
         selectedValues: undefined,
         values: [],
+        search: '',
     },
     initial: 'loading',
     states: {
         loading: {
             invoke: {
                 src: (_, event) => {
-                    return fakeAPI(event.data);
+                    return fakeAPI({ search: event.data, page: 0 });
                 },
                 onDone: {
                     target: 'loaded',
-                    actions: assign({ values: (_: any, event) => event.data }),
+                    actions: assign({ values: (_: any, event) => event.data, currentPageNumber: 0 }),
+                }
+            },
+        },
+        loadingMore: {
+            invoke: {
+                src: (context) => {
+                    return fakeAPI({ search: context.search, page: ++context.currentPageNumber });
+                },
+                onDone: {
+                    target: 'loaded',
+                    actions: assign({
+                        values: (context: any, event) => context.values.concat(event.data),
+                        currentPageNumber: (context) => ++context.currentPageNumber,
+                    }),
                 }
             },
         },
         loaded: {
             on: {
-                SEARCH: 'loading',
+                SEARCH: {
+                    actions: (context, event) => {
+                        context.search = event.data;
+                    },
+                    target: 'loading'
+                },
+                LOAD_MORE: 'loadingMore',
             },
         },
         valueSelected: {
@@ -50,6 +72,8 @@ function App() {
     const [search, setSearch] = useState('');
     const [state, send] = useMachine(toggleMachine);
     const isLoading = state.matches('loading');
+    const isLoadingMore = state.matches('loadingMore');
+
 
     return (
         <div>
@@ -61,11 +85,20 @@ function App() {
             </div>
             {isLoading ?
                 <p>Loading....</p> :
-                <ul>
-                    {state.context.values.map((value, index) => (
-                        <li key={index}>{value}</li>
-                    ))}
-                </ul>}
+                <>
+                    <ul>
+                        {state.context.values.map((value, index) => (
+                            <li key={index}>{value}</li>
+                        ))}
+                    </ul>
+                    {isLoadingMore ?
+                        <p>Loading more...</p> :
+                        <button type="button" onClick={() => {
+                            send('LOAD_MORE');
+                        }}>Load more</button>
+                    }
+                </>
+            }
         </div>
     );
 }
